cf3e3d05682f5b81da20da59e81a287efc523167
jswhh <jszw@jszw.de>
2024-01-14 14:52:52 +0100

    Exception handling korrigiert, Kein Absturz bei Umlaten

--- alertcentral.py
+++ alertcentral.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Version 0.2
+# Version 0.2.2
 
 from Credentials import *
 
@@ -7,6 +7,7 @@ import paho.mqtt.client as mqtt_client
 import json
 import smtplib, ssl
 import email.message
+import socket
 
 client_id = "alertcentral"
 topic = "ALERT"
@@ -17,11 +18,14 @@ def send_mail(receiver_email, subject, text):
     message.set_payload(text)
     context = ssl.create_default_context()
     print(f"Sending mail to {receiver_email}") # as {message}")
-    
-    with smtplib.SMTP_SSL(mail_smtp_server, mail_smtp_port, context=context) as server:
-        server.login(mail_sender, mail_pwd)
-        server.sendmail(mail_sender, receiver_email, message.as_string()) 
-    
+
+    try:
+        with smtplib.SMTP_SSL(mail_smtp_server, mail_smtp_port, context=context) as server:
+            server.login(mail_sender, mail_pwd)
+            server.sendmail(mail_sender, receiver_email, message.as_string().encode("utf-8")) 
+    except (socket.gaierror):
+        print('Error trying to send Email!')
+        
 # Connect to MQTT
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -45,7 +49,7 @@ def subscribe(client: mqtt_client):
             # print(data["severity"], data["device"], data["message"])
             send_mail("jszw@jszw.de", data["severity"]+" in "+alert_location, "Device: "+data["device"]+"\n"+"Message: "+data["message"])
         except (json.decoder.JSONDecodeError, KeyError):  
-            print('Decoding JSON has failed')
+            print('Error: Decoding JSON has failed')
 
     client.subscribe(topic)
     client.on_message = on_message
